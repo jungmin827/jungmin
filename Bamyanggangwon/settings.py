928d5b770a71ba30b6c45ef450b601b4936f0c2e
JMjina <165762129+JMjina@users.noreply.github.com>
2024-05-07 16:56:40 +0900

    Update settings.py

--- Bamyanggangwon/settings.py
+++ Bamyanggangwon/settings.py
@@ -57,6 +57,11 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
+# CORS 추가
+CORS_ORIGIN_WHITELIST = (
+    'http://127.0.0.1:8000', 'http://localhost:3000')
+CORS_ALLOW_CREDENTIALS = True
+
 ROOT_URLCONF = 'Bamyanggangwon.urls'
 
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
