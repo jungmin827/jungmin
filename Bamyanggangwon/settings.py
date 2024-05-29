ec2c9b797eb0494ad3f9315010dbf004e8566ce4
jungmin827 <121072020+jungmin827@users.noreply.github.com>
2024-05-29 16:52:56 +0900

    Update settings.py

--- Bamyanggangwon/settings.py
+++ Bamyanggangwon/settings.py
@@ -59,8 +59,12 @@ MIDDLEWARE = [
 ]
 
 # CORS 추가
-CORS_ORIGIN_WHITELIST = (
-    'http://127.0.0.1:8000', 'http://localhost:3000')
+#CORS_ORIGIN_WHITELIST = (
+ #   'http://127.0.0.1:8000', 'http://localhost:3000')
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
+
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'Bamyanggangwon.urls'
