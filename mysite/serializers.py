4e58f2100db32e736ebe44cc9674fe6c6a7bc180
jungmin827 <121072020+jungmin827@users.noreply.github.com>
2024-06-10 20:37:30 +0900

    Update serializers.py

--- mysite/serializers.py
+++ mysite/serializers.py
@@ -13,8 +13,37 @@ from .models import UserKeyword
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password', 'birthday', 'gender', 'profile_pic']
 
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            email=validated_data['email'],
+            birthday=validated_data['birthday'],
+            gender=validated_data['gender'],
+            profile_pic=validated_data.get('profile_pic')
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.birthday = validated_data.get('birthday', instance.birthday)
+        instance.gender = validated_data.get('gender', instance.gender)
+        instance.profile_pic = validated_data.get('profile_pic', instance.profile_pic)
+
+        password = validated_data.get('password')
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
+    
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
@@ -44,7 +73,7 @@ class RegionSerializer(serializers.ModelSerializer):
 class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
-        fields = '__all__' 
+        fields = ['id', 'place_id']
 
 class TestQSerializer(serializers.ModelSerializer):
     class Meta:
